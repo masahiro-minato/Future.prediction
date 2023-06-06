2cd9f1edeacdcd821bd604986b01dbdebccd726f
masahiro-minato <masahirominato@gmail.com>
2023-06-06 14:51:43 +0900

    新規作成

---
+++ stan/bsts-AR-tvc-rd-smooth-pred-poisson-periphe-matrix.stan
@@ -0,0 +1,191 @@
+// DGLMによるポアソン分布時系列分析 
+// 再パラメータ化による収束性の向上を図っている
+// 複数データのサンプリングを可能にしている
+// 複数データごとに説明変数である稼働台数を設定可能である
+
+functions {
+  // 並列処理用関数
+  real partial_sum_lpmf (array[] int slice_y, int start, int end, vector lambda){
+    return poisson_log_lpmf(slice_y | lambda[start:end]);
+  }
+}
+
+data {
+  int T;                              // データ取得期間の長さ（時系列数）
+  int N;                              // データ数（項目数）
+  int pred_term;                      // 予測期間の長さ
+  array[N,T] int<lower=0> y;          // 観測値 （Nは項目数,Tは時系列数）
+  // int y[N,T];                      // 観測値 旧表記
+  array[N] vector<lower=0>[T] Num;    // 稼働台数
+  // array[N,T] real<lower=0> Num;       // 稼働台数
+  array[N] real<lower=0> N_max;       // 稼働台数を正規化時の除算数
+  array[N] real<lower=0> Num_gain;    // 増加台数予測値（1ステップ当たりの増加予測数）
+  int<lower=1> grainsize;             // 粒度　250?
+}
+
+parameters {
+  vector<lower=0>[N] s_z;      // ドリフト成分の変動の大きさを表す標準偏差
+  vector<lower=0>[N] s_s;      // 季節変動の大きさを表す標準偏差
+  vector<lower=0>[N] s_r;      // ランダム効果の標準偏差
+  vector<lower=0>[N] s_t;      // 時変係数（稼働台数）の標準偏差
+  vector[N] b_ar;              // 自己回帰項の係数
+  vector[N] Intercept;         // 切片
+  array[N] vector[T] r_raw;           // ランダム効果の再パラメータ化
+  array[N] vector[T] mu_err;          // 水準成分の増減量
+  array[N] vector[T] b_ope_err;       // 稼働台数項の係数の増減量
+  array[N] vector[T-6] gamma_err;     // 季節成分の推定値
+}
+
+transformed parameters {
+  array[N] vector[T] mu;              // 水準+ドリフト成分の推定値
+  array[N] vector[T] b_ope;           // 稼働台数項の係数
+  array[N] vector[T] lambda;          // 観測値の期待値のlogをとった値
+  // vector[T] lambda[N];             // 観測値の期待値のlogをとった値 旧表記
+  array[N] vector[T-6] sumgamma;      // 周期成分
+  array[N] vector[T] tvc;             // 時変係数*稼働台数成分
+  array[N] vector[T] r;               // ランダム効果
+  array[N] vector[T] gamma;           // 季節成分の推定値
+  
+  for (n in 1:N){
+    // ランダム効果成分 r
+    r[n,] = s_r[n] * r_raw[n,];   // 再パラメータ化
+    // 平準化トレンドモデル水準成分 μ, 稼働台数時変係数 b_ope
+    // 1時点目
+    mu[n,1] = Intercept[n] + s_z[n] * mu_err[n,1];
+    b_ope[n,1] = s_t[n] * b_ope_err[n,1];
+    // 2時点目
+    mu[n,2] = Intercept[n] + b_ar[n] * mu[n,1] + s_z[n] * mu_err[n,2];
+    // 2時点目以降
+    for (i in 2:T) {
+      b_ope[n,i] = b_ope[n,(i-1)] + s_t[n] * b_ope_err[n,i];  // 再パラメータ化
+    }
+    // 3時点目以降
+    for (i in 3:T) {
+      mu[n,i] = Intercept[n] + b_ar[n] * (2 * mu[n,(i-1)] - mu[n,(i-2)]) + s_z[n] * mu_err[n,i];  // 再パラメータ化
+    }
+    // 周期成分 γ
+    gamma[n,1:6] = s_s[n] * gamma_err[n,1:6];
+    for(i in 1:T-6) {
+      sumgamma[n,i] = -sum(gamma[n,(i):(i+5)]);
+      gamma[n,(i+6)] = sumgamma[n,i] + s_s[n] * gamma_err[n,i]; // 再パラメータ化
+    }
+    // 時変係数成分
+    for(i in 1:T) {
+      tvc[n,i] = b_ope[n,i] * Num[n,i];
+    }
+    // 状態推定値 λ
+    lambda[n,1:T] = mu[n,1:T] + gamma[n,1:T] + tvc[n,1:T] + r[n,1:T];
+  }
+}
+
+model {
+  // 弱情報事前分布
+  s_z ~ student_t(5, 0, 1);
+  s_s ~ student_t(5, 0, 1);
+  s_r ~ student_t(5, 0, 1);
+  s_t ~ student_t(5, 0, 1);
+  b_ar ~ student_t(3, 0, 1);
+  Intercept ~ student_t(3, 0, 10);
+  // データ数（N）ごとにサンプリングする
+  for (n in 1:N) {
+    // 時点ごとに加わるランダム効果
+    // r ~ normal(0, s_r);
+    r_raw[n,] ~ normal(0,1);
+    // 稼働台数の時変係数
+    // b_ope[2:T] ~ normal(b_ope[1:T-1], s_t);
+    b_ope_err[n,] ~ normal(0,1);
+    // 水準+自己回帰成分
+    // mu[3:T] ~ normal(Intercept + b_ar * (2 * mu[2:T-1] - mu[1:T-2]), s_w);
+    // mu_err[2]以降は、標準偏差1の正規乱数を得る
+    mu_err[n,] ~ normal(0,1);
+    // 季節成分
+    // gamma[7:T] ~ normal(sumgamma[1:T-6], s_s);
+    gamma_err[n,1:T-6] ~ normal(0,1);
+  }
+  // 観測方程式に従い観測値が得られる
+  // 並列処理
+  for (n in 1:N){
+    target += reduce_sum(partial_sum_lpmf, y[n,], grainsize, lambda[n,]);
+    // 並列処理を行わない場合
+    // y[n,] ~ poisson_log(lambda[n,]);
+  }
+}
+
+generated quantities {
+  // 予測範囲のパラメータ設定
+  array[N] vector[T+pred_term] mu_pred;                  // 水準+ドリフト成分の推定値
+  array[N] vector[T+pred_term] b_ope_pred;               // 稼働台数項の係数
+  array[N] vector[T+pred_term] lambda_pred;              // 観測値の期待値
+  array[N] vector[T-6+pred_term] sumgamma_pred;          // 周期成分
+  array[N] vector[T+pred_term] tvc_pred;                 // 時変係数*稼働台数成分
+  array[N] vector[T+pred_term] r_pred;                   // ランダム効果
+  array[N] vector[T+pred_term] gamma_pred;               // 季節成分の推定値
+  array[N] vector[T+pred_term] Num_pred;                          // 稼働台数
+
+  // データ取得期間は元の変数と同値なので,そのまま代入する
+  for (n in 1:N) {
+    mu_pred[n,1:T] = mu[n,1:T];
+    b_ope_pred[n,1:T] = b_ope[n,1:T];
+    lambda_pred[n,1:T] = lambda[n,1:T];
+    sumgamma_pred[n,1:(T-6)] = sumgamma[n,1:(T-6)];
+    tvc_pred[n,1:T] = tvc[n,1:T];
+    r_pred[n,1:T] = r[n,1:T];
+    gamma_pred[n,1:T] = gamma[n,1:T];
+    Num_pred[n,1:T] = Num[n,1:T];
+  }
+
+  // データ数（N）ごとに未来予測期間を算出
+  for (n in 1:N) {
+    for (i in 1:pred_term) {
+      // ランダム効果成分 r
+      r_pred[n,(T+i)] = normal_rng(0, s_r[n]);
+      // 水準成分 μ
+      mu_pred[n,(T+i)] = normal_rng(Intercept[n] + b_ar[n] * (2 * mu_pred[n,(T+i-1)] - mu_pred[n,(T+i-2)]), s_z[n]);
+      // 稼働台数時変係数 b_ope
+      b_ope_pred[n,(T+i)] = normal_rng(b_ope_pred[n,(T+i-1)], s_t[n]);
+      // 周期成分 γ
+      sumgamma_pred[n,(T-6+i)] = -sum(gamma_pred[n,(T-6+i):(T-6+i+5)]);
+      gamma_pred[n,(T+i)] = normal_rng(sumgamma_pred[n,(T-6+i)], s_s[n]);
+      // 想定稼働台数
+      Num_pred[n,T+i] = Num_pred[n,T+i-1] + Num_gain[n];
+      // 時変係数成分 tvc
+      tvc_pred[n,(T+i)] = b_ope_pred[n,(T+i)] * Num_pred[n,T+i];
+      // 状態推定値 λ
+      lambda_pred[n,(T+i)] = mu_pred[n,(T+i)] + gamma_pred[n,(T+i)] + tvc_pred[n,(T+i)] + r_pred[n,(T+i)];
+    }
+  }
+  // パラメータ設定
+  array[N] vector[T+pred_term] lambda_exp;             // 状態推定値(EXP)
+  array[N] vector[T+pred_term] mu_gamma_exp;           // ランダム効果を除いた状態推定値
+  array[N] vector[T+pred_term] mu_r_exp;               // 周期成分を除いた状態推定値
+  array[N] vector[T+pred_term] mu_tvc_exp;             // 水準成分+時変係数成分
+  array[N] vector[T+pred_term] mu_exp;                 // 周期成分・ランダム効果を除いた状態推定値
+  array[N] vector[T+pred_term] gamma_exp;              // 周期成分(EXP)
+  array[N] vector[T+pred_term-31] delta_exp;           // ドリフト成分(EXP)
+  array[N] vector[T+pred_term] r_exp;                  // ランダム成分(EXP)
+  array[N] vector[T+pred_term] tvc_exp;                // 時変係数*稼働台数成分
+  array[N] vector[T+pred_term] mu_tvc_1_exp;           // 稼働台数を1台に設定(EXP)
+  array[N] vector[T+pred_term] mu_tvc_1;               // 稼働台数を1台に設定
+  array[N] vector[T+pred_term] tvc_1_exp;              // 稼働台数時変係数(EXP)
+  array[N] vector[T+pred_term] tvc_1;                  // 稼働台数時変係数,EM率相当
+  array[N,T+pred_term] int<lower=0> y_pred;            // 観測値の予測値
+
+  for (n in 1:N) {
+    // 指数関数を作用させる
+    lambda_exp[n,] = exp(lambda_pred[n,]);
+    mu_gamma_exp[n,] = exp(mu_pred[n,] + gamma_pred[n,]);
+    mu_r_exp[n,] = exp(mu_pred[n,] + r_pred[n,]);
+    mu_tvc_exp[n,] = exp(mu_pred[n,] + tvc_pred[n,]);
+    mu_exp[n,] = exp(mu_pred[n,]);
+    gamma_exp[n,] = exp(gamma_pred[n,]);
+    delta_exp[n,1:(T+pred_term-31)] = exp(mu_pred[n,32:(T+pred_term)]-mu_pred[n,31:(T-1+pred_term)]);
+    r_exp[n,] = exp(r_pred[n,]);
+    tvc_exp[n,] = exp(tvc_pred[n,]);
+    mu_tvc_1_exp[n,] = exp(mu_pred[n,] + b_ope_pred[n,] / N_max[n]);
+    mu_tvc_1[n,] = mu_pred[n,] + b_ope_pred[n,] / N_max[n];
+    tvc_1_exp[n,] = exp(b_ope_pred[n,] / N_max[n]);
+    tvc_1[n,] = b_ope_pred[n,] / N_max[n];
+    // 観測値の予測値算出
+    y_pred[n,] = poisson_log_rng(lambda_pred[n,]);
+  }
+}
---
+++ 周辺機別時系列モデル未来予測.R
@@ -0,0 +1,391 @@
+#### EM件数の基本構造時系列モデルでの推定 ####
+
+# ファイル読込
+MF3_maintenance <- read_tsv("./tsv_data/Metis_MF3_2211.tsv")　    # 保守データ
+MF3_machine <- read_tsv("./tsv_data/Metis_MIF_2211.tsv")          # 機器データ
+
+# 周辺機名称
+MF3_maintenance %>% distinct(Peripheral_name) %>% dput()
+# 周辺機名称
+MF3_maintenance$Peripheral_name
+# 列名
+names(MF3_maintenance)
+# 周辺機機種ごとの時系列でのEM件数の抽出
+EM.count.MF3.Peripheral <- 
+  MF3_maintenance %>% 
+  dplyr::filter((Peripheral_name %in% c("COOK-C", "SINAI-H") & Treatment_location %in% c("ADF部")) | 
+                (Peripheral_name %in% c("AMUR-C(HY)", "AMUR-C中綴じ","VOLGA-E") & Treatment_location %in% c("ﾊﾟﾝﾁ部","ﾌｨﾆｯｼｬｰ/ｿｰﾀｰ部"))) %>%
+  group_by(Maintenance_date, Peripheral_name) %>%
+  summarise(
+    EM.count = n()
+  ) %>% 
+  ungroup() %>% 
+  pivot_wider(names_from = c(Peripheral_name),
+              values_from = EM.count) %>% 
+  mutate_all(~replace(., is.na(.), 0)) %>% 
+  # 列名の変更
+  set_colnames(c("Maintenance_date", "EM.SINAI", "EM.VOLGA", "EM.COOK", "EM.AMUR", "EM.AMUR.HY")) %>%
+  # 不要列の削除
+  select("Maintenance_date", "EM.COOK", "EM.SINAI", "EM.VOLGA", "EM.AMUR", "EM.AMUR.HY")
+
+# 市場機台数
+MIF.count.MF3.Peripheral <- 
+  MF3_machine %>% 
+  group_by(納品年月日,ADF,フィニッシャー) %>% 
+  summarise(
+    N.date = n()
+  ) %>% 
+  # tidyr::drop_na() %>% # この行があると後処理がない場合のADFも削除される
+  ungroup() %>% 
+  pivot_wider(names_from = c(ADF,フィニッシャー),
+              values_from = N.date) %>% 
+  mutate_all(~replace(., is.na(.), 0)) %>% 
+# 周辺機機種ごとの市場機台数を集計/機種名の含まれる列の行合計を計算する
+# MIF.count.MF3.Peripheral <- 
+#   MIF.count.MF3.Peripheral %>% 
+  mutate(
+    MIF.COOK = select(.,contains("COOK-C")) %>% rowSums(na.rm=TRUE),
+    MIF.SINAI = select(.,contains("SINAI-H")) %>% rowSums(na.rm=TRUE),
+    MIF.VOLGA = select(.,contains("VOLGA-E")) %>% rowSums(na.rm=TRUE),
+    MIF.AMUR = select(.,contains("AMUR-C中綴じ")) %>% rowSums(na.rm=TRUE),
+    MIF.AMUR.HY = select(.,contains("AMUR-C(HY)")) %>% rowSums(na.rm=TRUE)
+  ) %>% 
+  select(納品年月日, MIF.COOK, MIF.SINAI, MIF.VOLGA, MIF.AMUR, MIF.AMUR.HY)
+
+# 結合
+X.date.MF3 <- seq(as.POSIXct("2019-01-01"), as.POSIXct("2022-11-30"), by = "day")
+MIF.date.MF3.Peripheral <- tibble(X.date = X.date.MF3)
+MF3.MIF_by.date <- 
+  MIF.date.MF3.Peripheral %>% 
+  full_join(MIF.count.MF3.Peripheral, by=c("X.date" = "納品年月日")) %>% 
+  full_join(EM.count.MF3.Peripheral, by=c("X.date" = "Maintenance_date")) %>% 
+  mutate_if(is.numeric, ~replace(., is.na(.), 0)) %>% 
+  # 市場機台数を累積和へ変換
+  mutate_at(vars(contains("MIF")), cumsum)
+
+# データの準備
+start_day = which(MF3.MIF_by.date$X.date == "2019-02-01 JST")  # "2020-01-01 JST"
+end_day = which(MF3.MIF_by.date$X.date == "2022-11-30 JST")
+pred_term = 30 # 予測期間
+data.num <- end_day - start_day + 1 + pred_term # 予測期間を含むデータ数
+# EM観測値のみをマトリクスへ変換
+names(MF3.MIF_by.date)
+# 列番号の取得
+X.date.colnum <- which(names(MF3.MIF_by.date) == "X.date")
+# サンプリング期間におけるEM観測値をマトリクスへ変換と転置
+y <- t(as.matrix(MF3.MIF_by.date[start_day:end_day,][,c(-X.date.colnum, -c(2:6))]))
+# 稼働台数
+Num.tib <- MF3.MIF_by.date[start_day:end_day,][,c(-X.date.colnum, -c(7:11))]
+# 正規化のために最大値を算出
+max_data <-
+  Num.tib %>%
+  summarize(across(where(is.numeric), \(x) max(x, na.rm = TRUE))) # R4.2.3以降
+max_data$MIF.COOK
+print(max_data)
+# ベクトルへの変換
+unlist(c(max_data))
+# 正規化
+for(col in names(max_data)){
+  print(col)
+  print(eval(parse(text = paste0("max_data$",col))))
+  Num.tib <- Num.tib %>%
+    mutate(
+      !!col :=eval(parse(text = paste0("Num.tib$",col)))/eval(parse(text = paste0("max_data$",col)))
+    )
+}
+# マトリクスへ変換と転置
+Num <- t(as.matrix(Num.tib))
+# データリストの作成
+data_list <- list(
+  y = y,
+  N = 5,
+  Num = Num, # 正規化が必要
+  T = end_day-start_day+1,
+  N_max = unlist(c(max_data)),
+  grainsize = 250,
+  pred_term = pred_term,
+  Num_gain = c(200,20,100,50,20)/unlist(c(max_data)) # 月次での稼働台数増加予測数
+)
+# コンパイル
+mod <- cmdstan_model("./stan/bsts-AR-tvc-rd-smooth-pred-poisson-periphe-matrix.stan", cpp_options = list(stan_threads = TRUE))
+# マルチコア対応
+options(mc.cores = parallel::detectCores())
+# path
+title <- str_c("Metis-MF3-Peripheral")
+output_dir <- str_c("./csv/matrix")
+output_basename <-str_c(title,".",format(Sys.time(), "%H-%M-%S"))
+object.path <- str_c("./Cmdstan_files/matrix/",title,".EM.pred.Peripheral.matrix.fit-",start_day,"~",end_day,".rds")
+# csv保存ディレクトリーの作成
+if(!dir.exists(output_dir)){
+  dir.create(output_dir)}
+# MCMCサンプリング
+exTime <- system.time(
+  fit <- mod$sample(data = data_list,
+                    seed = 1234,
+                    chains = 6,
+                    parallel_chains = getOption("mc.cores", 24),
+                    threads_per_chain = 2,
+                    iter_warmup = 4000, #10000,
+                    iter_sampling = 2000, #5000,
+                    thin = 4,
+                    # adapt_delta = 0.90,
+                    max_treedepth = 12,
+                    refresh = 500,
+                    init = 0, # エラー回避
+                    output_dir = output_dir, # csvデータ保存ディレクトリ
+                    output_basename = output_basename, # csvデータ保存名称
+                    show_messages = FALSE
+  )
+)
+
+# 実行時間
+exTimeTable <- data.frame(user.self = exTime["user.self"], 
+                          sys.self = exTime["sys.self"],
+                          elapsed = exTime["elapsed"],
+                          title = title,
+                          row.names = "time")
+exTimeTable
+# 保存
+write_tsv(exTimeTable, str_c("./time/exTimeTable.",title,".",start_day,"~",end_day,".tsv"))
+# 結果保存
+fit$save_object(file = object.path)
+# サンプリング結果表示
+fit$print(c( "s_z","s_s", "s_r", "s_t", "b_ar", "b_ope[1,1]", "b_ope[2,2]","b_ope[3,3]","Intercept", "lp__"), max_rows=100)
+# rhatヒストグラム
+fit %>% bayesplot::rhat() %>% hist(main=str_c("Histogram of ",title))
+# 時系列グラフ用ｘ軸データ
+time_vec <- 
+  seq(
+    from = as.POSIXct("2019-02-01 JST"),
+    by = "days",
+    len = data.num # end_day-start_day+1+pred_term
+  )
+length(time_vec)
+
+names(MF3.MIF_by.date)[2:6]
+
+# 表示パラメータの設定
+parm <- "y_pred"
+# 表示パラメータの抽出
+parm.tib <- 
+  as_tibble(fit$draws(parm) %>% as_draws_df)
+# 分位数の算出
+parm.quan <- 
+  apply(parm.tib, 2, function(i){quantile(i,prob=c(0.025, 0.5, 0.975))})
+parm.quan <- 
+  as_tibble(parm.quan) %>% 
+  # 不要列の削除
+  select(-.chain, -.iteration, -.draw )
+# 周辺機名称ベクトルの作成
+Peripheral.MF3 <- substring(names(EM.count.MF3.Peripheral)[-1], 4,)
+# グラフ表示用データフレームの作成
+result_df <- 
+  as_tibble(t(parm.quan),.name_repair = 'unique') %>% 
+  mutate(
+    Peripheral = rep(Peripheral.MF3, data.num)
+  ) %>% 
+  # 列名設定
+  set_colnames(c("lwr", "fit", "upr", "Peripheral"))
+# 時間軸項の追加
+result_df$time <- 
+  rep(time_vec, times = rep(length(Peripheral.MF3), length(time_vec)))
+# レベル設定
+result_df$Peripheral <- 
+  factor(result_df$Peripheral, levels=Peripheral.MF3)
+# DFとFINへ区分
+result_df.ADF <- 
+  result_df %>% 
+  dplyr::filter(Peripheral %in% c("COOK","SINAI"))
+result_df.FIN <- 
+  result_df %>% 
+  dplyr::filter(Peripheral %in% c("VOLGA","AMUR","AMUR.HY"))
+
+# b_ar 自己回帰係数
+b_ar <- 
+  fit$draws("b_ar") %>% 
+  as_draws_df
+# b_ar平均値
+b_ar.mean <- round(fit$draws("b_ar") %>% apply(3,mean),3)
+print(b_ar.mean)
+
+# 測定値データ
+MF3.EM_by.date <- 
+  MF3.MIF_by.date %>% 
+  select(-starts_with("MIF.")) %>%
+  set_colnames(c("X.date",Peripheral.MF3)) %>% 
+  pivot_longer(cols = all_of(Peripheral.MF3),
+               names_to = "Peripheral",
+               values_to = "EM.num")
+MF3.EM_by.date$Peripheral <- 
+  factor(MF3.EM_by.date$Peripheral, levels = Peripheral.MF3)
+# ADFのみ抽出
+MF3.EM_by.date.ADF <- 
+  MF3.EM_by.date %>% 
+  dplyr::filter(Peripheral %in% c("COOK","SINAI"))
+# FINのみ抽出
+MF3.EM_by.date.FIN <- 
+  MF3.EM_by.date %>% 
+  dplyr::filter(Peripheral %in% c("VOLGA","AMUR","AMUR.HY"))
+
+# フォント定義
+par(family="Noto Sans")
+# グラフタイトル
+if (parm == "lambda_exp") {
+  graph_title <- str_c("Metis-MF3 EM件数時系列分析 λ(μ + γ + tvf + r)：すべての成分を含んだ状態推定値")
+  fill.graph <- "lightblue"
+}else if (parm == "y_pred") {
+  graph_title <- str_c("Metis-MF3 EM件数時系列分析 95%予測区間")
+  fill.graph <- "lightgreen"
+}
+# 縦軸名称
+y_label = "件数"
+# 凡例設定
+vec <- c()
+for (i in 1:length(Peripheral.MF3)) {
+  vec <- 
+    vec %>% 
+    append(str_c(i,".",Peripheral.MF3[i]," (自己回帰係数 b_ar = ",b_ar.mean[i],")"))
+}
+vec.ADF <- c()
+for (i in 1:2) {
+  vec.ADF <- 
+    vec.ADF %>% 
+    append(str_c(Peripheral.MF3[i]," (自己回帰係数 b_ar = ",b_ar.mean[i],")"))
+}
+vec.FIN <- c()
+for (i in 3:5) {
+  vec.FIN <- 
+    vec.FIN %>% 
+    append(str_c(Peripheral.MF3[i]," (自己回帰係数 b_ar = ",b_ar.mean[i],")"))
+}
+# ADF+FIN
+Att.labs <- vec 
+names(Att.labs) <- Peripheral.MF3
+# グラフの表示期間の設定
+limits = c(as.POSIXct("2022-04-01 JST"), as.POSIXct(MF3.MIF_by.date$X.date[end_day] + 3600*24*pred_term))
+# ｙ軸目盛設定
+breaks=seq(0,1000,50)
+# 図示
+p <- ggplot(data = result_df, aes(x = time)) + 
+  theme_bw() + 
+  labs(title = graph_title) +
+  theme(plot.title = element_text(size = 18,  #font size and adjust
+                                  hjust = 0.01,#adjust
+  )) +
+  ylab(y_label) +
+  geom_ribbon(aes(ymin = lwr, ymax = upr), alpha = 0.4, fill=fill.graph) + 
+  geom_line(aes(y = fit), linewidth = 1.2) +
+  geom_point(alpha = 1.0, size = 0.9,
+             data = MF3.EM_by.date, mapping =  aes(x = X.date, y = EM.num, color = Peripheral)) +
+  theme(axis.title.x = element_text(size = 16),
+        axis.title.y = element_text(size = 16),
+        axis.text.x = element_text(angle = 30, hjust = 1, size = 14),
+        axis.text.y = element_text(size = 16)) +
+  theme(legend.position = "none") +
+  theme(strip.background = element_blank(), strip.text = element_text(size = 16, hjust = 0)) +
+  facet_wrap(~Peripheral, ncol = 1, labeller = labeller(Peripheral = Att.labs)) +
+  geom_vline(xintercept = as.POSIXct("2022-11-30 JST"), linetype = 2, color = "darkblue") + # 垂直線
+  scale_x_datetime(limits = limits, date_breaks = "1 month", date_labels = "%Y/%m") + # 表示期間の設定
+  scale_y_continuous(breaks = breaks) +
+  # 未来予測期間の文字表示
+  annotate("text", x = as.POSIXct("2022-11-30 JST"), y = Inf, 
+           hjust = -0.01, # 文字のｘ方向位置調整 マイナスで右へ移動
+           vjust = 2, 　　# 文字のｙ方向位置調整 プラスで下へ移動
+           label = "⇒ 【未来予測区間】", 
+           size = 5, 
+           colour = "darkblue")
+
+plot(p)
+
+# 予測区間+取得データグラフ保存
+n <- 5
+ggsave(str_c("./PDF/",title,".EM件数 予測区間-",start_day,"~",end_day,".pdf"),
+       plot = p, device = cairo_pdf, dpi=300, width=20, height=(n*3))
+
+# ADF
+Att.labs <- vec.ADF
+names(Att.labs) <- Peripheral.MF3[1:2]
+# グラフの表示期間の設定
+limits = c(as.POSIXct("2022-04-01 JST"), as.POSIXct(MF3.MIF_by.date$X.date[end_day] + 3600*24*pred_term))
+# ｙ軸目盛設定
+breaks=seq(0,1000,50)
+# 図示
+p.ADF <- ggplot(data = result_df.ADF, aes(x = time)) + 
+  theme_bw() + 
+  labs(title = graph_title) +
+  theme(plot.title = element_text(size = 18,  #font size and adjust
+                                  hjust = 0.01,#adjust
+  )) +
+  ylab(y_label) +
+  geom_ribbon(aes(ymin = lwr, ymax = upr), alpha = 0.4, fill=fill.graph) + 
+  geom_line(aes(y = fit), linewidth = 1.2) +
+  geom_point(alpha = 1.0, size = 0.9,
+             data = MF3.EM_by.date.ADF, mapping =  aes(x = X.date, y = EM.num, color = Peripheral)) +
+  theme(axis.title.x = element_text(size = 16),
+        axis.title.y = element_text(size = 16),
+        axis.text.x = element_text(angle = 30, hjust = 1, size = 14),
+        axis.text.y = element_text(size = 16)) +
+  theme(legend.position = "none") +
+  theme(strip.background = element_blank(), strip.text = element_text(size = 16, hjust = 0)) +
+  facet_wrap(~Peripheral, ncol = 1, labeller = labeller(Peripheral = Att.labs)) +
+  geom_vline(xintercept = as.POSIXct("2022-11-30 JST"), linetype = 2, color = "darkblue") + # 垂直線
+  scale_x_datetime(limits = limits, date_breaks = "1 month", date_labels = "%Y/%m") + # 表示期間の設定
+  scale_y_continuous(breaks = breaks) +
+  # 未来予測期間の文字表示
+  annotate("text", x = as.POSIXct("2022-11-30 JST"), y = Inf, 
+           hjust = -0.01, # 文字のｘ方向位置調整 マイナスで右へ移動
+           vjust = 2, 　　# 文字のｙ方向位置調整 プラスで下へ移動
+           label = "⇒ 【未来予測区間】", 
+           size = 5, 
+           colour = "darkblue")
+
+plot(p.ADF)
+
+# 予測区間+取得データグラフ保存
+n <- 2
+ggsave(str_c("./PDF/",title,".ADF_EM件数 予測区間-",start_day,"~",end_day,".pdf"), 
+       plot = p.ADF, device = cairo_pdf, dpi=300, width=20, height=(n*5))
+
+# FIN
+Att.labs <- vec.FIN
+names(Att.labs) <- Peripheral.MF3[3:5]
+# グラフの表示期間の設定
+limits = c(as.POSIXct("2022-04-01 JST"), as.POSIXct(MF3.MIF_by.date$X.date[end_day] + 3600*24*pred_term))
+# ｙ軸目盛設定
+breaks=seq(0,100,2)
+# 図示
+p.FIN <- ggplot(data = result_df.FIN, aes(x = time)) + 
+  theme_bw() + 
+  labs(title = graph_title) +
+  theme(plot.title = element_text(size = 18,  #font size and adjust
+                                  hjust = 0.01,#adjust
+  )) +
+  ylab(y_label) +
+  geom_ribbon(aes(ymin = lwr, ymax = upr), alpha = 0.4, fill=fill.graph) + 
+  geom_line(aes(y = fit), linewidth = 1.2) +
+  geom_point(alpha = 1.0, size = 0.9,
+             data = MF3.EM_by.date.FIN, mapping =  aes(x = X.date, y = EM.num, color = Peripheral)) +
+  theme(axis.title.x = element_text(size = 16),
+        axis.title.y = element_text(size = 16),
+        axis.text.x = element_text(angle = 30, hjust = 1, size = 14),
+        axis.text.y = element_text(size = 16)) +
+  theme(legend.position = "none") +
+  theme(strip.background = element_blank(), strip.text = element_text(size = 16, hjust = 0)) +
+  facet_wrap(~Peripheral, ncol = 1, labeller = labeller(Peripheral = Att.labs)) +
+  geom_vline(xintercept = as.POSIXct("2022-11-30 JST"), linetype = 2, color = "darkblue") + # 垂直線
+  scale_x_datetime(limits = limits, date_breaks = "1 month", date_labels = "%Y/%m") + # 表示期間の設定
+  scale_y_continuous(breaks = breaks) +
+  # 未来予測期間の文字表示
+  annotate("text", x = as.POSIXct("2022-11-30 JST"), y = Inf, 
+           hjust = -0.01, # 文字のｘ方向位置調整 マイナスで右へ移動
+           vjust = 2, 　　# 文字のｙ方向位置調整 プラスで下へ移動
+           label = "⇒ 【未来予測区間】", 
+           size = 5, 
+           colour = "darkblue")
+
+plot(p.FIN)
+
+# 予測区間+取得データグラフ保存
+n <- 3
+ggsave(str_c("./PDF/",title,".FIN_EM件数 予測区間-",start_day,"~",end_day,".pdf"), 
+       plot = p.FIN, device = cairo_pdf, dpi=300, width=20, height=(n*5))
