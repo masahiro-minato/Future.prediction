62192a05ddd0163b0230b8b8067e41c5e7ec264f
masahiro-minato <masahirominato@gmail.com>
2023-09-14 13:04:43 +0900

    新規作成

---
+++ MF-4未来予測関数実行.R
@@ -0,0 +1,35 @@
+fit <- MF4Future.pred(
+  # 入力データ/tsv形式
+  MF4_maintenance.tsv = "./tsv_data/Metis_MF4_2305.tsv",
+  MF4_machine.tsv = "./tsv_data/MF4_MIF_2305.tsv",
+  MF3_maintenance.tsv = "./tsv_data/Metis_MF3_2303.tsv",
+  MF3_machine.tsv = "./tsv_data/Metis_MIF_2303.tsv",
+  # MCMCサンプリング開始日と終了日
+  start_day.MF4 = "2023-03-01 JST",
+  end_day.MF4 = "2023-05-31 JST",
+  # 予測期間
+  pred_term = 30,
+  # 日次での稼働台数増加予測数 c(COOK,SINAI,VOLGA,AMUR中綴じ)
+  Num_gain = c(70,20,10,8),
+  # csv保存ディレクトリ
+  output_dir = "./csv/matrix",
+  # MCMCサンプリング
+  chains = 6,
+  parallel_chains = getOption("mc.cores", 24),
+  threads_per_chain = 2,
+  iter_warmup = 2000, #6000,
+  iter_sampling = 2000, #2000,
+  thin = 4,
+  max_treedepth = 12, #12
+  # タイトル
+  title = "Metis-MF4-Peripheral.MF3parm_tmp2",
+  # サンプリング時間保存
+  save_exTime = TRUE,
+  # サンプリング結果保存
+  save_fit = TRUE,
+  # グラフ保存
+  save_graph = TRUE)
+
+
+p <- readRDS("./rds/Metis-MF4-Peripheral.MF3parm_tmp2.EM件数_予測区間-29~120_[70,20,10,8].rds")
+p
